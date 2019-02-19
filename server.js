4b657118532fc395d180c179763b5c5efb1848d3
Teixeira Nunes Rogerio <TeixeiraR@corvallis.it>
2019-02-19 17:49:00 +0100

    add object to db

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
+const express = require('express');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+server.use('/public', express.static('public'));
 server.use(router);
 
 server.listen(port);
